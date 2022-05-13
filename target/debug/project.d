52b1fbc01cfa911aed3066403212e45b447410a0
Huilin Zhang <97477503+Orangeadee@users.noreply.github.com>
2022-05-12 20:42:14 -0700

    init

--- target/debug/project.d
+++ target/debug/project.d
@@ -1,1 +1,1 @@
-/Users/huilinzhang/Desktop/ECS_140A/project/target/debug/project: /Users/huilinzhang/Desktop/ECS_140A/project/src/cstream.rs /Users/huilinzhang/Desktop/ECS_140A/project/src/main.rs
+/Users/huilinzhang/Documents/GitHub/ECS140A_Project/target/debug/project: /Users/huilinzhang/Documents/GitHub/ECS140A_Project/src/cstream.rs /Users/huilinzhang/Documents/GitHub/ECS140A_Project/src/main.rs
